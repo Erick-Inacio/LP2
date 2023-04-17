6806a2b5a244bd747a63c4ac51fcd27903320b29
Erick F Inacio <erick.de.faria@hotmail.com>
2023-04-17 13:40:01 -0300

    Adicionando atv 4 finalizada e atv 7 em andamento

---
+++ Atividade7/PMenu/PMenu/frmExercicio3.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PMenu
+{
+    partial class frmExercicio3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInverte = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.lblPalavra2 = new System.Windows.Forms.Label();
+            this.lblPalavra1 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtPalavra2 = new System.Windows.Forms.TextBox();
+            this.txtPalavra1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInverte
+            // 
+            this.btnInverte.Location = new System.Drawing.Point(255, 129);
+            this.btnInverte.Name = "btnInverte";
+            this.btnInverte.Size = new System.Drawing.Size(102, 88);
+            this.btnInverte.TabIndex = 13;
+            this.btnInverte.Text = "Inverte palavra 1";
+            this.btnInverte.UseVisualStyleBackColor = true;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(144, 129);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(102, 88);
+            this.btnReplace.TabIndex = 12;
+            this.btnReplace.Text = "Remove ocorrências 1º no 2º (replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            // 
+            // lblPalavra2
+            // 
+            this.lblPalavra2.AutoSize = true;
+            this.lblPalavra2.Location = new System.Drawing.Point(30, 61);
+            this.lblPalavra2.Name = "lblPalavra2";
+            this.lblPalavra2.Size = new System.Drawing.Size(49, 13);
+            this.lblPalavra2.TabIndex = 11;
+            this.lblPalavra2.Text = "Palavra2";
+            // 
+            // lblPalavra1
+            // 
+            this.lblPalavra1.AutoSize = true;
+            this.lblPalavra1.Location = new System.Drawing.Point(30, 27);
+            this.lblPalavra1.Name = "lblPalavra1";
+            this.lblPalavra1.Size = new System.Drawing.Size(49, 13);
+            this.lblPalavra1.TabIndex = 10;
+            this.lblPalavra1.Text = "Palavra1";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(33, 129);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(102, 88);
+            this.btnRemove.TabIndex = 9;
+            this.btnRemove.Text = "Remove ocorrências 1º no 2º";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtPalavra2
+            // 
+            this.txtPalavra2.Location = new System.Drawing.Point(115, 58);
+            this.txtPalavra2.Name = "txtPalavra2";
+            this.txtPalavra2.Size = new System.Drawing.Size(100, 20);
+            this.txtPalavra2.TabIndex = 8;
+            // 
+            // txtPalavra1
+            // 
+            this.txtPalavra1.Location = new System.Drawing.Point(115, 21);
+            this.txtPalavra1.Name = "txtPalavra1";
+            this.txtPalavra1.Size = new System.Drawing.Size(100, 20);
+            this.txtPalavra1.TabIndex = 7;
+            // 
+            // frmExercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInverte);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.lblPalavra2);
+            this.Controls.Add(this.lblPalavra1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtPalavra2);
+            this.Controls.Add(this.txtPalavra1);
+            this.Name = "frmExercicio3";
+            this.Text = "frmExercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInverte;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label lblPalavra2;
+        private System.Windows.Forms.Label lblPalavra1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtPalavra2;
+        private System.Windows.Forms.TextBox txtPalavra1;
+    }
+}
